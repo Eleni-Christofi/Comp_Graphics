48d206babaaf0fa0891431fc00f92585d2fadfdc
Eleni Christofi <56637913+Eleni-Christofi@users.noreply.github.com>
2019-11-05 14:35:06 +0000

    Update sphere.h

--- my_version/sphere.h
+++ my_version/sphere.h
@@ -20,30 +20,9 @@ public:
 		radius = r;
 	}
 	
-	Vector get_norm(const Vector& pi) const { return (pi - c) / r; }
 
 	void intersection(Ray ray, Hit &hit);
 	{
-		const Vector p = ray.position;
-		const Vector d = ray.direction;
-		const Vector pc = p - Vector(c.x,c.y,c.z);
 		
-		const double b = 2 * pc.dot(d);
-		const double c = pc.dot(pc) - r * r;
-		
-		double disc = b * b - 4 * c;
-		if (disc < 1e-4) hit.flag = false;
-		
-		disc = sqrt(disc);
-		
-		const double t0 = -b - disc;
-		const double t1 = -b + disc;
-		hit.t = (t0 < t1) ? t0 : t1;
-		hit.flag = true;
-		
-		Vector pi = p = d * t;
-		hit.position = pi;
-		hit.normal = get_norm(pi);
-
 	}
 };
