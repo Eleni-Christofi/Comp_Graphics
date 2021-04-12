f00cd128439097e343825d44ec3940df8cbe7e99
Eleni Christofi <56637913+Eleni-Christofi@users.noreply.github.com>
2021-04-12 15:38:18 +0100

    Delete my_version directory

--- my_version/use_later.cpp
+++
@@ -1,9 +0,0 @@
-
-//const Sphere light(Vector(0, 0, 50), 1);
-//Vector L = light.c - hit.position;
-//L = L.normalise();
-//Vector N = hit.normal;
-//N = N.normalise();
-//float dt = L.dot(N);
-//hit.colour = white * dt;
-//clamp255(hit.colour);
